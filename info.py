7bfd5b2cb2a428f7eed1da5a91dd5bd7c3da48f4
MooFlix <makwanamakwana2005@gmail.com>
2025-06-29 00:01:23 +0530

    Update info.py

--- info.py
+++ info.py
@@ -60,8 +60,8 @@ DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in enviro
 
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "")   # IF Multiple Database Is False Then Fill Only This Database Url.
-DATABASE_NAME = environ.get('DATABASE_NAME', "techvjclonefilterbot")
-COLLECTION_NAME = environ.get('COLLECTION_NAME', 'vjcollection')
+DATABASE_NAME = environ.get('DATABASE_NAME', "MAKWANA'S ORG")
+COLLECTION_NAME = environ.get('COLLECTION_NAME', 'MooFlixcollection')
 
 MULTIPLE_DATABASE = bool(environ.get('MULTIPLE_DATABASE', False)) # Set True or False
 
