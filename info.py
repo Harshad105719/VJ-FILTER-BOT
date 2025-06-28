c07d6c6f56db3bcd44f5e648e6f38488761aca90
MooFlix <makwanamakwana2005@gmail.com>
2025-06-29 00:11:58 +0530

    Update info.py

--- info.py
+++ info.py
@@ -126,8 +126,8 @@ VERIFY_SND_SHORTLINK_API = environ.get('VERIFY_SND_SHORTLINK_API', '')
 
 # Shortlink Info
 SHORTLINK_MODE = bool(environ.get('SHORTLINK_MODE', False)) # Set True Or False
-SHORTLINK_URL = environ.get('SHORTLINK_URL', '')
-SHORTLINK_API = environ.get('SHORTLINK_API', '')
+SHORTLINK_URL = environ.get('SHORTLINK_URL', 'Earn4links.in')
+SHORTLINK_API = environ.get('SHORTLINK_API', '442d3d0cca12ef18712d1 585828d9e36c1655cf7')
 TUTORIAL = environ.get('TUTORIAL', 'https://t.me/MooFlixRobot?start=BQADAQADlQsAAmw44EYbjONcrKuwHRYE') # How Open Shortner Link Video Link , Channel Link Where You Upload Your Video.
 
 
